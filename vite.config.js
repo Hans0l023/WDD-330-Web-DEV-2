d5e066858db809ac2dddf9da6987d41469e36f9d
Hans0l023 <watts.hans@gmail.com>
2024-07-19 21:56:53 -0500

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         input: {
           main: resolve(__dirname, "src/index.html"),
           type: resolve(__dirname, "src/type/index.html"),
+          workout: resolve(__dirname, "src/workout/index.html"),
           user: resolve(__dirname, "src/user/index.html"),
         },
       },
